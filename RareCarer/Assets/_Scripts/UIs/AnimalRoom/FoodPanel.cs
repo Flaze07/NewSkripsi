c2081e64ae5b6db5aad70517bc832dfe4b06b8ac
StalemateZ <rrobertus02@yahoo.com>
2024-06-22 23:12:17 +0700

    Added food not enough panel

--- RareCarer/Assets/_Scripts/UIs/AnimalRoom/FoodPanel.cs
+++ RareCarer/Assets/_Scripts/UIs/AnimalRoom/FoodPanel.cs
@@ -12,13 +12,17 @@ public class FoodPanel : MonoBehaviour
     private FoodConsumable foodConsumablePrefab;
     [SerializeField]
     private GameObject foodConsumableParent;
-
     [SerializeField]
+    private GameObject notEnoughFoodPanel;
+
+        [SerializeField]
     private AudioSource audioSource;
     [SerializeField]
     private AudioClip eatingAudioClip;
+    [SerializeField]
+    private AudioClip notEnoughFoodAudioClip;
 
-    void OnEnable()
+        void OnEnable()
     {
         // create a new food consumable for each food item in the food manager
         foreach (var food in GameManager.instance.Foods)
@@ -30,6 +34,8 @@ public class FoodPanel : MonoBehaviour
             btn.onClick.AddListener(() => {
                 if(food.Amount == 0)
                 {
+                    notEnoughFoodPanel.SetActive(true);
+                    audioSource.PlayOneShot(notEnoughFoodAudioClip);
                     return;
                 }
                 GameManager.instance.CurrentAnimal.Feed(food.Type);
